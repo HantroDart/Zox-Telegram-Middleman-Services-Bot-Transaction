ac4144e25913e80c9bf2a402aa1ecef4190a4091
leonardoroot <darkgtsa12@outlook.com>
2026-03-07 17:24:05 +0300

    Upload Files

---
+++ Telegram-Middleman-Services-Bot/Data/ProductsType.cs
@@ -0,0 +1,23 @@
+﻿namespace TelegramShopBot
+{
+    internal class ProductsType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Product>? Products { get; set; }
+
+        public int CategoryId { get; set; }
+
+
+        public bool IsAnyOfProductsAvailableToBuy()
+        {
+            if (Products != null)
+            {
+                return Products.Where(p => p.IsEmployed == false).Count() != 0;
+            }
+
+            return false;
+        }        
+    }
+}
