9ce3adfcdd1eec1f51a4ccea87a1d909862d0668
leonardoroot <darkgtsa12@outlook.com>
2026-03-07 17:24:01 +0300

    Upload Files

---
+++ Telegram-Middleman-Services-Bot/Data/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace TelegramShopBot
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<ProductsType>? ProductsTypes { get; set; }
+        
+    }
+}
