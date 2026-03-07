29d59d07fd2fecad589714da7ab7e2ab89a4560a
leonardoroot <darkgtsa12@outlook.com>
2026-03-07 17:24:41 +0300

    Upload Files

---
+++ Telegram-Middleman-Services-Bot/Telegram/Menus/MenuWithAbilityToReturn.cs
@@ -0,0 +1,25 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramShopBot
+{
+    internal abstract class MenuWithAbilityToReturn : Menu
+    {
+        protected Func<ITelegramBotClient, Update, long, Task> returnToMainMenuAction;
+        public Func<ITelegramBotClient, Update, long, Task> ReturnToMainMenuAction
+        {
+            set
+            {
+                if (returnToMainMenuAction == null)
+                {
+                    returnToMainMenuAction = value;
+                }
+
+            }
+        }
+
+
+
+        protected MenuWithAbilityToReturn(string name) : base(name) { }
+    }
+}
