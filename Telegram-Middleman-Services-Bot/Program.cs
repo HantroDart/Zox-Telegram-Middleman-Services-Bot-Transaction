e92b69f25d1b70d79ecabd437d51885786dd6e0e
leonardoroot <darkgtsa12@outlook.com>
2026-03-07 17:23:56 +0300

    Upload Files

---
+++ Telegram-Middleman-Services-Bot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TelegramShopBot
+{
+    //CODENAME : OYA_Project
+
+    internal class Program
+    {
+        private const string token = "6455238621:AAEWpyzS7hB1LOz4yNGddn7tARmsEmU8xWg";
+
+
+
+        static void Main(string[] args)
+        {
+            using(DatabaseContext db = new DatabaseContext())
+            {
+                db.Database.EnsureCreated();
+            }
+            Console.WriteLine("DataBase Started");
+
+
+            TelegramBotManager telegramBotManager = new(token);
+            telegramBotManager.StartTelegramBot();
+            Console.WriteLine("Bot Started");
+
+
+            Console.ReadKey();
+            Console.WriteLine("End");
+        }
+    }
+}
