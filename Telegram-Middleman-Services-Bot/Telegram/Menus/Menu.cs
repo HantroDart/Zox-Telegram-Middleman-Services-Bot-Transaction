8c1f5ab4cc5fed8ecde196edc3066706b2de8990
leonardoroot <darkgtsa12@outlook.com>
2026-03-07 17:24:39 +0300

    Upload Files

---
+++ Telegram-Middleman-Services-Bot/Telegram/Menus/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramShopBot
+{
+    abstract class Menu
+    {
+        public string Name { get; private set; }
+
+        protected const string returnButtonText = "↩️Back↩️";
+
+
+
+        public Menu(string name)
+        {
+            Name = name;
+        }
+
+
+
+        public abstract Task CreateMenuGovernMessageAsync(ITelegramBotClient botClient, Update update, long chatId);
+    }
+}
