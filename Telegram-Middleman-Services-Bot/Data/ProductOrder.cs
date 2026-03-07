26d095e5d9c7ee894831ed188042ea87f1c3ad18
leonardoroot <darkgtsa12@outlook.com>
2026-03-07 17:24:03 +0300

    Upload Files

---
+++ Telegram-Middleman-Services-Bot/Data/ProductOrder.cs
@@ -0,0 +1,17 @@
+﻿namespace TelegramShopBot
+{
+    internal class ProductOrder
+    {
+        public int Id { get; set; }
+        public DateTime? TimeOfCreating { get; set; }
+        public Category? Category { get; set; }
+        public ProductsType? ProductType { get; set; }
+        public Product? Product { get; set; }
+
+        public ShopClient? ShopClientOnCreating { get; set; }
+        public long? ShopClientIdOnCreating { get; set; }
+
+        public ShopClient? ShopClientOnStoring { get; set; }
+        public long? ShopClientIdOnStoring { get; set; }
+    }
+}
