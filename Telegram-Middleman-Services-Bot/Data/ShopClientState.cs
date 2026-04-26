ed22e5c34fb69f19aef75539eab8e3c9dedd9bad
YoskoBG <darkgt31252@outlook.com>
2026-04-26 17:45:51 +0300

    Upload Files

---
+++ Telegram-Middleman-Services-Bot/Data/ShopClientState.cs
@@ -0,0 +1,21 @@
+﻿namespace TelegramShopBot
+{
+    public enum ShopClientState
+    {
+        BeforeMainMenuCreating,
+        MenuChoising,
+        SimpleMessageReading,
+        CategoryChoising,
+        ProductsTypeChoising,
+        ProductChoising,
+        ProductInfoReading,
+        AfterPayingForTheProduct,
+        AccountMenuChoising,
+        AboutAccountMessageReading,
+        DeleteAccountWarningMessageReading,
+        EnteringTheDepositAmount,
+        AfterTopUpBalance,
+        ProductOrderChoising,
+        ProductOrderInfoReading
+    }
+}
