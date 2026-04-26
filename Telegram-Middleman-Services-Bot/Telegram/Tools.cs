7cb89d90c42dafccce4c84d7e36c7a08de87ed77
YoskoBG <darkgt31252@outlook.com>
2026-04-26 17:46:17 +0300

    Upload Files

---
+++ Telegram-Middleman-Services-Bot/Telegram/Tools.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace TelegramShopBot
+{
+    internal static class Tools
+    {
+        public static string GetMessageTextFromTxt(string path)
+        {
+            using (StreamReader sr = new StreamReader(Environment.CurrentDirectory + @"\Telegram\Messages\" + path, Encoding.Default))
+            {
+                string readText = sr.ReadToEnd();
+                return readText;
+            }
+        }
+    }
+}
