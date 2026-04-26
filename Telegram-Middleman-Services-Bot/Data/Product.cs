eb09e765068981f068a6f5798cd5ec0c43c2b9a0
YoskoBG <darkgt31252@outlook.com>
2026-04-26 17:45:45 +0300

    Upload Files

---
+++ Telegram-Middleman-Services-Bot/Data/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace TelegramShopBot
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+
+
+        public string SubscribeTime { get; set; }
+        public decimal Price { get; set; }
+
+
+        public string Mail { get; set; }
+        public string MailPassword { get; set; }
+        public string AccountPassword { get; set; }
+
+
+        public bool IsEmployed { get; set; }
+    }
+}
