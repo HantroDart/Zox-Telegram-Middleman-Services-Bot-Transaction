e3b11cda8bf7146036d73222f72657b324549064
YoskoBG <darkgt31252@outlook.com>
2026-04-26 17:46:26 +0300

    Upload Files

---
+++ Telegram-Middleman-Services-Bot/Telegram/Menus/SimpleMenu.cs
@@ -0,0 +1,56 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramShopBot
+{
+    internal class SimpleMenu : Menu
+    {
+        private readonly string messageText;
+
+
+
+        public SimpleMenu(string name, string messageText) : base(name)
+        {
+            this.messageText = messageText;
+        }
+
+
+
+        public override async Task CreateMenuGovernMessageAsync(ITelegramBotClient botClient, Update update, long chatId)
+        {
+            int controlMessageId;
+
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                ShopClient shopClient = db.ShopClients.FirstOrDefault(sc => sc.ChatId == chatId);
+
+                shopClient.State = ShopClientState.SimpleMessageReading;
+                controlMessageId = (int)shopClient.ControlMessageId;
+
+                await db.SaveChangesAsync();
+            };
+
+            await botClient.EditMessageTextAsync(
+                chatId: chatId,
+                messageId: controlMessageId,
+                text: messageText,
+                replyMarkup: CreateGovernMessageKeyboard());
+        }
+
+        private InlineKeyboardMarkup CreateGovernMessageKeyboard()
+        {
+            InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup(
+                new[]
+                {
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData(returnButtonText)
+                    }
+                });
+
+            return keyboard;
+        }
+
+    }
+}
